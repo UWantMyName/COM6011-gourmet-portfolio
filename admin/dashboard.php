a9b9a9098ca5de49a3baf18251c0343c503593e9
UWantMyName <stefantanasa13@gmail.com>
2025-07-05 19:43:21 +0300

    Refactor the dashboard.

---
+++ admin/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+// admin/dashboard.php
+include __DIR__ . '/../inc/header.php';
+?>
+
+<div class="container" style="padding-top:6rem;" data-aos="fade-up">
+  <h1>Admin Dashboard</h1>
+
+  <div class="admin-buttons" data-aos="fade-up">
+    <a href="chefs.php"   class="btn">Manage Chefs</a>
+    <a href="recipes.php" class="btn">Manage Recipes</a>
+  </div>
+</div>
+
+<?php include __DIR__ . '/../inc/footer.php'; ?>
